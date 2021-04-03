50ed3e02cfbcc1e19956e68220c825211c3e90aa
Julianchopcode <julian.mora95@gmail.com>
2021-04-02 22:28:25 -0500

    Actualización filtros json

--- function.php
+++ function.php
@@ -1,11 +1,11 @@
 <?php
 function allresource(){
-  $data = file_get_contents("data-1.json");
-  $products = json_decode($data, true);
-
-  foreach ($products as $product) {
-      echo '<pre>';
-      print_r($product);
-      echo '</pre>';
-  }
+  // $data = file_get_contents("data-1.json");
+  // $products = json_decode($data, true);
+  //
+  // foreach ($products as $product) {
+  //     echo '<pre>';
+  //     print_r($product);
+  //     echo '</pre>';
+  // }
 }
--- js/app.js
+++ js/app.js
@@ -6,31 +6,72 @@ $(document).ready(function() {
       dataType:'json',
       success:function(data){
           for(d in data){
-            // console.log(data[d].Direccion);
-            // console.log(data[d].Ciudad);
-            // console.log(data[d].Telefono);
-            // console.log(data[d].Codigo_Postal);
-            // console.log(data[d].Tipo);
-            // console.log(data[d].Precio);
-              cad+=("<div class='row'>"+
-                      "<div class='col-sm-6 col-md-6 colorbackground'>" +
-                          "<img class='imgforsale' src='http://localhost/proyecto%20nextu/buscadorphp/img/home.jpg'>"+
-                        "</div>"+
-                        "<div class='col-sm-6 col-md-6 colorbackground'>" +
-                          "<div class='thumbnail'>"+
-                            "<div class='caption'>"+
-                              "</br>"+
-                              "<p><b> Dirección: </b>"+data[d].Direccion+"</p>"+
-                              "<p> " + data[d].Ciudad + "...</p>"+
-                              "<p><a href='#' class='btn btn-success' role='button'>Ver post</a></p>"+
+            console.log(data[d].Ciudad);
+                  cad+=("<div class='row'>"+
+                          "<div class='col-sm-6 col-md-6 colorbackground'>" +
+                              "<img class='imgforsale' src='http://localhost/proyecto%20nextu/buscadorphp/img/home.jpg'>"+
                             "</div>"+
-                          "</div>"+
-                        "</div>"+
-                    "</div>"
-                  );
+                            "<div class='col-sm-6 col-md-6 colorbackground'>" +
+                              "<div class='thumbnail'>"+
+                                "<div class='caption'>"+
+                                  "</br>"+
+                                  "<p><b> Dirección: </b>"+data[d].Direccion+"</p>"+
+                                  "<p><b> ciudad: </b> " + data[d].Ciudad + "</p>"+
+                                  "<p><b> Teléfono: </b> " + data[d].Telefono + "</p>"+
+                                  "<p><b> Código Postal: </b> " + data[d].Codigo_Postal + "</p>"+
+                                  "<p><b> Tipo: </b> " + data[d].Tipo + "</p>"+
+                                  "<p><b> Precio: </b> " + data[d].Precio + "</p>"+
+                                  "<hr>"+
+                                  "<p class='alingright'><a href='#' class='btn btn-success' role='button'>Ver post</a></p>"+
+                                "</div>"+
+                              "</div>"+
+                            "</div>"+
+                        "</div>"
+                      );
+              }
+               $('#contenedor').html(cad);
           }
-           $('#contenedor').html(cad);
-      }
-  });
+      });
    });
+
+   $( "form" ).submit(function( event ) {
+      var dataArray = $( this ).serializeArray();
+      dataObj = {};
+      $(dataArray).each(function(i, field){
+        dataObj[field.name] = field.value;
+      });
+     var cad = "";
+     $.ajax({
+       url:'data-1.json',
+       dataType:'json',
+       success:function(data){
+        const datacity = data.filter((d => d.Ciudad === dataObj['ciudad']) || (d => d.Tipo === dataObj['Tipo']) || (d => d.precio === dataObj['precio']));
+        console.log(datacity);
+           for(d in datacity){
+                   cad+=("<div class='row'>"+
+                           "<div class='col-sm-6 col-md-6 colorbackground'>" +
+                               "<img class='imgforsale' src='http://localhost/proyecto%20nextu/buscadorphp/img/home.jpg'>"+
+                             "</div>"+
+                             "<div class='col-sm-6 col-md-6 colorbackground'>" +
+                               "<div class='thumbnail'>"+
+                                 "<div class='caption'>"+
+                                   "</br>"+
+                                   "<p><b> Dirección: </b>"+datacity[d].Direccion+"</p>"+
+                                   "<p><b> ciudad: </b> " + datacity[d].Ciudad + "</p>"+
+                                   "<p><b> Teléfono: </b> " + datacity[d].Telefono + "</p>"+
+                                   "<p><b> Código Postal: </b> " + datacity[d].Codigo_Postal + "</p>"+
+                                   "<p><b> Tipo: </b> " + datacity[d].Tipo + "</p>"+
+                                   "<p><b> Precio: </b> " + datacity[d].Precio + "</p>"+
+                                   "<hr>"+
+                                   "<p class='alingright'><a href='#' class='btn btn-success' role='button'>Ver post</a></p>"+
+                                 "</div>"+
+                               "</div>"+
+                             "</div>"+
+                         "</div>"
+                       );
+              }
+               $('#contenedor').html(cad);
+           }
+       });
+  });
 });
